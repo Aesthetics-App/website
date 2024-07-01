622fab63dc8073aa322b00bb957102cea3e454af
David BELBEZE <david.belbeze@gmail.com>
2024-07-01 22:53:08 +0200

    Add showcase content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from './header/header.component';
+import { ShowcaseComponent } from './showcase/showcase.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShowcaseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
