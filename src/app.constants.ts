2d4f0787ec5cb39aef281e162dd291ec91215348
David BELBEZE <david.belbeze@gmail.com>
2024-07-06 16:42:17 +0200

    Add missing meta data

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -3,5 +3,8 @@ export const APP_CONSTANTS = {
     link: 'https://play.google.com/store/apps/details?id=com.aesthetics.workout',
     version: '1.7.1',
     publicationDate: new Date(Date.parse('2018-01-01')),
+    rating: 4.1,
+    ratingCount: 27,
+    url: 'https://aesthetics-app.web.app/',
   },
 };
