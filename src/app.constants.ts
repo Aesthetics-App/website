9ceb40055c45d52d2a656229a9f350da0bc5cce2
David BELBEZE <david.belbeze@gmail.com>
2024-07-14 10:45:45 +0200

    Update Android app version

--- src/app.constants.ts
+++ src/app.constants.ts
@@ -1,7 +1,7 @@
 export const APP_CONSTANTS = {
   android: {
     link: 'https://play.google.com/store/apps/details?id=com.aesthetics.workout',
-    version: '1.7.1',
+    version: '1.7.2',
     publicationDate: new Date(Date.parse('2018-01-01')),
     rating: 4.1,
     ratingCount: 27,
